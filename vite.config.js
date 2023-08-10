1fc8360bdd9226e056cedb8f799c988660f8aa3b
Maarlenf <marlenefigueroa.a22@gmail.com>
2023-08-10 19:18:44 -0400

    change config vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     VitePWA({
       registerType: "prompt",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
